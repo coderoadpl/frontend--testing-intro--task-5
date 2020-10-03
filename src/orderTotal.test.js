9e130c04a3d62d978d68731db93d47965e399889
Mateusz Choma <kontakt@coderoad.pl>
2020-10-03 11:09:46 +0200

    First orderTotal test in Jest added

--- src/orderTotal.test.js
+++ src/orderTotal.test.js
@@ -1,3 +1,11 @@
-test('should pass', () => {
+import orderTotal from './orderTotal'
+
+it('should pass', () => {
     expect(true).toBe(true)
+})
+
+it('should calculate total of empty cat', () => {
+    const cart = []
+
+    expect(orderTotal(cart)).toBe(0)
 })
